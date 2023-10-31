6c16f4e04fd863078dace7c8102179cd4b391f1b
sohith18 <sohith.gutta@gmail.com>
2023-10-31 22:11:19 +0530

    Added front-end work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,32 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-import { Component } from 'react';
 import Navbar from '../src/components/Navbar';
 import Home from '../src/pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import Video from './pages/meeting';
-import JoinRoom from "./pages/join";
 import axios from 'axios';
 import {Toaster } from 'react-hot-toast'
+import Medicine from './pages/Medicines';
 
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true;
 
-class App extends Component {
-    render(){
+function App () {
     return (
-        <>
-        <Navbar />
-        <Toaster position='bottom-right' toastOptions={{duration:2000}}/>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path= "/video-call-room" component ={JoinRoom} />
-            <Route path= "/video/:id" component={Video} />
-
-        </Routes>
-        </>
+        <div className='App'>
+            <Navbar />
+            <Toaster position='bottom-right' toastOptions={{duration:2000}}/>
+            <div className='Content'>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/medicines" element={<Medicine/>}/>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+            </div>
+        </div>
+        
     );
-    }
 }
 
 export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,4 +4,5 @@ export default function Home() {
   return (
     <div>Home</div>
   )
+  
 }
