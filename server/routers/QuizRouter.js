08cffa9553e18cf318fa6ff0a0466f70e4c753eb
Aaryan-Ajith-Dev <aaryan.dev@iiitb.ac.in>
2024-11-01 12:31:51 +0530

    fixed auth

---
+++ server/routers/QuizRouter.js
@@ -0,0 +1,64 @@
+import express from 'express'
+import { createQuiz, getQuiz, updateQuiz, deleteQuiz, updateAnswers, deleteAttemptedQuiz } from '../services/QuizService.js';
+import { getUserFromToken } from '../services/UserService.js';
+
+const QuizRouter = express.Router();
+
+QuizRouter.put('/', async (req, res) => {
+    const header = req.headers;
+    const token = header.authorization.substring(7);
+    const userRes = await getUserFromToken(token);
+    const quiz = req.body;
+    // creating a quiz logic
+    let response = null;
+    response = await createQuiz(quiz, userRes.user)
+    res.status(response.status)
+    res.send(response)
+})
+
+
+QuizRouter.get('/attempt/:id', async (req, res) => {
+    let response = null;
+    response = await getQuiz(req.params.id)
+    res.status(response.status)
+    res.send(response)
+})
+
+QuizRouter.delete('/attempt/:quiz_id/:index', async (req, res) => {
+    let response = null;
+    const header = req.headers;
+    const token = header.authorization.substring(7);
+    const userRes = await getUserFromToken(token);
+    response = await deleteAttemptedQuiz(req.params.index, req.params.quiz_id, userRes.user)
+    res.status(response.status)
+    res.send(response)
+})
+
+QuizRouter.post('/attempt', async (req, res) => {   // need to test
+    let response = null;
+    const header = req.headers;
+    const token = header.authorization.substring(7);
+    const userRes = await getUserFromToken(token);
+    response = await updateAnswers(req.body.answers, userRes.user, req.body.quiz);
+    res.status(response.status)
+    res.send(response)
+})
+
+QuizRouter.post('/', async (req, res) => {
+    let response = null;
+    response = await updateQuiz(req.body)
+    res.status(response.status)
+    res.send(response)
+})
+
+QuizRouter.delete('/:id', async (req, res) => { // need to test
+    let response = null;
+    const header = req.headers;
+    const token = header.authorization.substring(7);
+    const userRes = await getUserFromToken(token);
+    response = await deleteQuiz(req.params.id, userRes.user)
+    res.status(response.status)
+    res.send(response)
+})
+
+export default QuizRouter;
